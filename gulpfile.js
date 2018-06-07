76328c8a4516f7a904ff79bf4826f32a3eeeab88
illia osmanov <illiaosmanov@gmail.com>
2018-06-07 15:06:05 +0300

    added sources

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+const gulp = require('gulp');
+const nunjucks = require('gulp-nunjucks-render');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync');
+const plumber = require('gulp-plumber');
+
+gulp.task('scss', () => {
+  return gulp.src('src/scss/main.scss')
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(gulp.dest('dist/css/'))
+    .pipe(browserSync.stream())
+});
+
+gulp.task('html', () => {
+  return gulp.src('src/views/*.html')
+    .pipe(nunjucks({
+      path: 'src/',
+    }))
+    .pipe(gulp.dest('dist/'))
+    .pipe(browserSync.stream())
+});
+
+gulp.task('reload', () => {
+  browserSync({
+    server: {
+      baseDir: 'dist/'
+    },
+    notify: false,
+  });
+});
+
+gulp.task('watch', ['reload', 'scss', 'html'], () => {
+  gulp.watch('src/scss/**/*.scss', ['scss']);
+  gulp.watch(['src/views/*.html', 'src/components/**/*.html'], ['html'])
+  gulp.watch('dist/*.html', browserSync.reload)
+});
